476f6af6f795237c181a5728133fd24cd251b6d3
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-05-29 15:38:05 +0200

    adding initial google map

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -7,7 +7,6 @@
 Route::get('/', 'HomeController@index')->name('index');
 Route::get('contact', 'ContactController@index')->name('contact');
 Route::post('contact/send', 'ContactController@send')->name('contact.send');
-
 /*
  * These frontend controllers require the user to be logged in
  * All route names are prefixed with 'frontend.'
