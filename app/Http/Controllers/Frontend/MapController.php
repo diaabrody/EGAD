6aa4e3563a7f910b11e50a665e09604808a49b6c
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-05-30 18:47:25 +0200

    creating Locations table, model, finishing add marker functionality

--- app/Http/Controllers/Frontend/MapController.php
+++ app/Http/Controllers/Frontend/MapController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Frontend;
 use Cornford\Googlmapper\Facades\MapperFacade as Mapper;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Location;
 
 class MapController extends Controller
 {
@@ -12,10 +14,41 @@ class MapController extends Controller
      * @throws \Cornford\Googlmapper\Facades\MapperException
      */
     public function index(Request $request){
-//        dd($request->search);
-        $search = $request->search ? $request->search : 'Alexandria';
-        Mapper::location($search)->map( [ 'zoom' => 15 ,  'marker' => true ]);
+        $search = $request->search ? $request->search : 'Egypt';
+        $locations = Location::all();
+        Mapper::location($search)->map( [ 'clusters' => ['size' => 10, 'center' => true , 'zoom' => 20 ] ,  'marker' => true ]);
+
+        foreach($locations as $location)
+        {
+            Mapper::marker($location->location->getLat(), $location->location->getLng(), ['symbol' => 'circle', 'scale' => 1000]);
+
+        }
+
 
         return view('frontend.maps.map');
     }
+
+
+    public function create(Request $request)
+    {
+        $marker =  $request->marker;
+
+        $lat =  Mapper::location($marker)->getLatitude();
+        $lng = Mapper::location($marker)->getLongitude();
+
+        $place = new Location();
+
+        $place->name = $request->marker;
+        $place->report_id = 1;
+
+        $place->location = new Point($lat, $lng);	// (lat, lng)
+
+        $place->save();
+
+
+        return redirect('/map');
+
+    }
+
+
 }
