953d85e8680688dc1d68a34b01680d4ccadbb410
nadadahab <nadadahab906@gmail.com>
2018-05-30 18:43:53 +0200

    Show Report

---
+++ resources/views/frontend/reports/index.blade.php
@@ -0,0 +1,23 @@
+<table border="1px" class="table table-hover">
+<tr>
+<th>Report Number</th>
+<th>User Name</th>
+<th>actions</th>
+</tr>
+@foreach ($reports as $report)
+<tr>
+<td>
+{{ $report->id }} 
+</td>
+<td>
+{{ $report->user->name }}
+</td>
+<td>
+<a href="reports/{{ $report->id }}"  name="view" class="btn btn-info"> view</a>
+</td>
+</tr>
+
+</table>
+</div>
+
+@endforeach
