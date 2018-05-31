6cfd81a65bb5666984471f26aaeccd5409fe37e9
WessamSaeid <wessam.saeid33@gmail.com>
2018-05-31 15:28:05 +0200

    modifing email attribute validation

--- app/Http/Requests/Frontend/Auth/RegisterRequest.php
+++ app/Http/Requests/Frontend/Auth/RegisterRequest.php
@@ -31,7 +31,7 @@ class RegisterRequest extends FormRequest
         return [
             'first_name'           => 'required|string|max:191',
             'last_name'            => 'required|string|max:191',
-            'email'                => ['required', 'string', 'email', 'max:191', Rule::unique('users')],
+            'email'                => ['required', 'string', 'email', 'max:191'],
             'phone_no'             => ['required', 'regex:/(01)[0-9]{9}/', Rule::unique('users')],
             'password'             => 'required|string|min:6|confirmed',
             'g-recaptcha-response' => ['required_if:captcha_status,true', new CaptchaRule()],
