e8c268e09f5c4fd9d3d96a99aa5a8700f03955a1
Yara-SalahElDine <yarasalah94@hotmail.com>
2018-06-05 07:33:58 +0200

    add twitter feed

---
+++ app/Http/Controllers/Frontend/Tweet/TweetController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Tweet;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Twitter;
+
+
+
+
+class TweetController extends Controller
+{
+
+
+    public function index()
+    {
+        $tweets1 = Twitter::getSearch(['q' => 'اطفال_مفقودة#', 'count' => '100', 'format' => 'array']);
+        $tweets2 = Twitter::getSearch(['q' => 'اطفال_مخطوفة#', 'count' => '100', 'format' => 'array']);
+        //dd($tweets2);
+        return view('frontend.tweets.index',[
+            'tweets1' => $tweets1,
+            'tweets2' => $tweets2
+        ]);
+    }
+
+   
+}
