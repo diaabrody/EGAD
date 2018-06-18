406ca3b2693de6f675c60f019aa8c24fbdfe8fa0
diaabrody <diaa_osama70@outlook.com>
2018-06-18 21:14:43 +0200

    edit

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -20,6 +20,7 @@
             <link href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/components/button.min.css" rel="stylesheet">
             <link href="{{ asset('/vendor/laravelLikeComment/css/style.css') }}" rel="stylesheet">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 
             @yield('meta')
 
