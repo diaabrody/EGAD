e5444d963ce7ecb46e5f5cab66a00bb662ecadf3
diaabrody <diaa_osama70@outlook.com>
2018-06-18 14:42:49 +0200

    make auto complete in egypt only

--- public/js/location-spinner.js
+++ public/js/location-spinner.js
@@ -13,7 +13,7 @@ function initAutocomplete() {
     // location types.
     autocomplete = new google.maps.places.Autocomplete(
         /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-        {types: ['geocode']});
+        {types: ['geocode'] ,  componentRestrictions: {country: 'EG'}} );
 
     // When the user selects an address from the dropdown, populate the address
     // fields in the form.
